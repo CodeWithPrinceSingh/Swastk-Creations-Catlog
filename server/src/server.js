a13953667e3340f861599847175e187bf735f0f3
CodeWithPrinceSingh <111429747+CodeWithPrinceSingh@users.noreply.github.com>
2026-06-24 19:17:37 +0530

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,8 @@
 import dotenv from 'dotenv';
 dotenv.config();
-const sitemapRoute = require("./routes/sitemap");
+
 import app from './app.js';
-app.use("/", sitemapRoute);
+
 import { connectMongo } from './utils/db.js';
 
 const PORT = process.env.PORT || 5000;
