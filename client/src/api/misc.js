c062362ff7f4df47335fa00cc05514c082f50095
CodeWithPrinceSingh <111429747+CodeWithPrinceSingh@users.noreply.github.com>
2026-06-27 16:20:12 +0530

    Add files via upload

--- client/src/api/misc.js
+++ client/src/api/misc.js
@@ -2,6 +2,18 @@ import api from './client.js';
 
 export const fetchTestimonials = () => api.get('/testimonials').then((res) => res.data);
 
+export const submitTestimonial = (payload) =>
+  api.post('/testimonials', payload).then((res) => res.data);
+
+export const fetchPendingTestimonials = () =>
+  api.get('/testimonials/pending').then((res) => res.data);
+
+export const approveTestimonial = (id) =>
+  api.patch(`/testimonials/${id}/approve`).then((res) => res.data);
+
+export const deleteTestimonial = (id) =>
+  api.delete(`/testimonials/${id}`).then((res) => res.data);
+
 export const fetchDashboardStats = () => api.get('/admin/dashboard').then((res) => res.data);
 
 export const fetchAllUsers = () => api.get('/admin/users').then((res) => res.data);
