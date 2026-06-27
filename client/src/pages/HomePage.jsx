4898ff784175a3374bc0453546680902d83b158d
CodeWithPrinceSingh <111429747+CodeWithPrinceSingh@users.noreply.github.com>
2026-06-27 14:41:33 +0530

    Update HomePage.jsx

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -69,7 +69,7 @@ export default function HomePage() {
         <QuizBanner />
       </Reveal>
 
-      <ExclusiveBanner />
+     
 
       <Reveal>
         <Testimonials testimonials={testimonials} />
