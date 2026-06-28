4a4e4c9f2f691c082862d06c94156788a14be97a
CodeWithPrinceSingh <111429747+CodeWithPrinceSingh@users.noreply.github.com>
2026-06-28 20:24:02 +0530

    Update robotsController.js

--- server/src/controllers/robotsController.js
+++ server/src/controllers/robotsController.js
@@ -1,10 +1,40 @@
 export const getRobotsTxt = (req, res) => {
   const SITE_URL = (process.env.CLIENT_ORIGIN || 'https://swastik-creations-catlog.vercel.app').replace(/\/$/, '');
 
-  const content = `User-agent: *
-Allow: /
-
-Sitemap: ${SITE_URL}/sitemap.xml
+  const content = `User-agent: Googlebot
+Disallow: 
+User-agent: googlebot-image
+Disallow: 
+User-agent: googlebot-mobile
+Disallow: 
+User-agent: MSNBot
+Disallow: 
+User-agent: Slurp
+Disallow: 
+User-agent: Gigabot
+Disallow: 
+User-agent: Robozilla
+Disallow: 
+User-agent: Nutch
+Disallow: 
+User-agent: ia_archiver
+Disallow: 
+User-agent: baiduspider
+Disallow: 
+User-agent: naverbot
+Disallow: 
+User-agent: yeti
+Disallow: 
+User-agent: yahoo-mmcrawler
+Disallow: 
+User-agent: psbot
+Disallow: 
+User-agent: yahoo-blogs/v3.9
+Disallow: 
+User-agent: *
+Disallow:
+  
+  Sitemap: ${SITE_URL}/sitemap.xml
 `;
 
   res.set('Content-Type', 'text/plain');
