84b27256ffd2ec6985b8f6ce176839a7b56762ce
CodeWithPrinceSingh <111429747+CodeWithPrinceSingh@users.noreply.github.com>
2026-06-30 13:48:43 +0530

    Update GalleryPage.jsx

--- client/src/pages/GalleryPage.jsx
+++ client/src/pages/GalleryPage.jsx
@@ -1,12 +1,5 @@
 const galleryImages = [
-  'https://instagram.fdel27-3.fna.fbcdn.net/v/t51.827…OeCJLRrl1WCjhlKo9S3uk0k1kZBukXcrTwwYg&oe=6A494529',
-  'https://images.unsplash.com/photo-1599643478518-a784e5dc4c8f?w=600',
-  'https://images.unsplash.com/photo-1583391733956-6c78276477e2?w=600',
-  'https://images.unsplash.com/photo-1522335789203-aabd1fc54bc9?w=600',
-  'https://images.unsplash.com/photo-1543163521-1bf539c55dd2?w=600',
-  'https://images.unsplash.com/photo-1591561954557-26941169b49e?w=600',
-  'https://images.unsplash.com/photo-1620656798579-1984d9e87df7?w=600',
-  'https://images.unsplash.com/photo-1549465220-1a8b9238cd48?w=600',
+  'https://instagram.fdel27-3.fna.fbcdn.net/v/t51.827…OeCJLRrl1WCjhlKo9S3uk0k1kZBukXcrTwwYg&oe=6A494529'
 ];
 
 export default function GalleryPage() {
