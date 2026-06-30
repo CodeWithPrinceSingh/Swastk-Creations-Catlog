fae9112c4bcb625f9d4058754f30af70802ffb1d
CodeWithPrinceSingh <111429747+CodeWithPrinceSingh@users.noreply.github.com>
2026-06-30 13:08:49 +0530

    Update Hero.jsx

--- client/src/components/home/Hero.jsx
+++ client/src/components/home/Hero.jsx
@@ -49,7 +49,7 @@ export default function Hero() {
   const slide = slides[index];
 
   return (
-    <section className="relative h-[620px] sm:h-[600px] overflow-hidden">
+    <section className="relative h-[600px] sm:h-[650px] overflow-hidden">
       {slides.map((s, i) => (
         <img
           key={i}
